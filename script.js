eef235a29995c4c1c6e789b493794cc46ff2a831
Jesse Smith <cappellacoding@gmail.com>
2024-10-02 13:21:25 +0200

    updated nav, sections, and stylling

--- script.js
+++ script.js
@@ -33,4 +33,18 @@ document.addEventListener("DOMContentLoaded", function () {
       hoverBox.style.display = "none";
     });
   });
+
+  // Smooth scrolling for nav links
+  const navLinks = document.querySelectorAll("nav ul li a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    });
+  });
 });
