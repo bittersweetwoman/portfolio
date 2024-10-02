dc455fcc470a2d4c1affb7af4e57e561407508d7
Jesse Smith <cappellacoding@gmail.com>
2024-10-03 00:28:09 +0200

    updated styling for mobile responsiveness, added mobile menu, and overall improvements

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Smooth scrolling for nav links
-  const navLinks = document.querySelectorAll("nav ul li a");
+  const navLinks = document.querySelectorAll(
+    "nav ul li a, .mobile-nav ul li a"
+  );
   navLinks.forEach((link) => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
@@ -45,6 +47,46 @@ document.addEventListener("DOMContentLoaded", function () {
         top: targetElement.offsetTop,
         behavior: "smooth",
       });
+      document.getElementById("mobile-nav").style.display = "none";
+      document.getElementById("menu-icon").src =
+        "./assets/icons/closed-book.svg";
     });
   });
+
+  // Mobile menu toggle
+  const menuIcon = document.getElementById("menu-icon");
+  const mobileNav = document.getElementById("mobile-nav");
+  const closeMenuButton = document.getElementById("close-menu");
+
+  menuIcon.addEventListener("click", function () {
+    mobileNav.style.display = "block";
+    menuIcon.src = "./assets/icons/open-book.png";
+  });
+
+  closeMenuButton.addEventListener("click", function () {
+    mobileNav.style.display = "none";
+    menuIcon.src = "./assets/icons/closed-book.png";
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!mobileNav.contains(e.target) && e.target !== menuIcon) {
+      mobileNav.style.display = "none";
+      menuIcon.src = "./assets/icons/closed-book.png";
+    }
+  });
+
+  // function adjustFeaturedHeight() {
+  //   const aboutContainer = document.querySelector(".about-container");
+  //   const featuredContainer = document.querySelector(".featured-container");
+
+  //   if (window.innerWidth < 1000) {
+  //     featuredContainer.style.height = `${aboutContainer.offsetHeight}px`;
+  //     featuredContainer.style.overflowY = "scroll";
+  //   } else {
+  //     featuredContainer.style.height = "auto";
+  //   }
+  // }
+
+  // window.addEventListener("resize", adjustFeaturedHeight);
+  // adjustFeaturedHeight(); // Initial call
 });
